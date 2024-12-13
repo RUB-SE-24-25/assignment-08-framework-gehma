9f4a9a4c872f9d0dcaed1e6e2761e4d06006a645
LeoHer <herlitz.leonhard@gmail.com>
2024-12-13 01:23:21 +0100

    Schmerzen ich Habe schmerzen
    Haja waowi

--- src/main/java/structra/assignment/task/impl/QuizWindow.java
+++ src/main/java/structra/assignment/task/impl/QuizWindow.java
@@ -3,10 +3,13 @@ package structra.assignment.task.impl;
 import javax.swing.*;
 import javax.swing.plaf.basic.DefaultMenuLayout;
 import java.awt.*;
+import java.awt.event.ActionEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
 public class QuizWindow {
+
+    private JButton newQuestion;
 
     public QuizWindow() {
         QuizWindowGenerator();
@@ -38,72 +41,32 @@ public class QuizWindow {
 
 
     // create new panel for the answers
-    JPanel answerPanel = new JPanel();
+        JPanel answerPanel = getAnswerPanel();
 
-        answerPanel.setBackground(Color.LIGHT_GRAY);
-
-    // create grid Bag layout so i cas set up the styling
-
-    answerPanel.setLayout(new GridBagLayout());
-    GridBagConstraints gbc = new GridBagConstraints();
-        gbc.insets = new Insets(5, 5, 5, 5);
-
-    // the text area where the questions are getting into after Button click toDo
-    JTextArea answerArea = new JTextArea("new Ansewers Herer");
-        answerArea.setFont(new Font("Arial", Font.PLAIN, 15));
-        answerArea.setEditable(false);
-        answerArea.setLineWrap(true);
-        answerArea.setWrapStyleWord(true);
-        answerArea.setFocusable(false);
-
-        JScrollPane scrollPane = new JScrollPane(answerArea);
-
-        // setting up the constraints for the Layout
-        gbc.gridx = 0;
-        gbc.gridy = 0;
-        gbc.gridwidth = 2; // Span across both columns
-        gbc.fill = GridBagConstraints.BOTH;
-        gbc.weightx = 1.0;
-        gbc.weighty = 0.90;
-
-    answerPanel.add(scrollPane, gbc);
-
-    JTextField answerField = new JTextField("enter your answer here");
-
-        answerField.setFont(new Font("Arial", Font.PLAIN, 20));
-
-        gbc.gridx = 0;
-        gbc.gridy = 1; // Same row as the submit button
-        gbc.gridwidth = 1; // Span only 1 column for answer field
-        gbc.weightx = 0.8; gbc.weighty=0.1;
-        gbc.fill = GridBagConstraints.BOTH;
-
-    answerPanel.add(answerField, gbc);
-
-    JButton answerButton = new JButton("submit");
-        answerButton.setFont(new Font("Arial", Font.PLAIN, 20));
-        answerButton.setBackground(Color.decode("#40629f")); // blue
-        answerButton.setForeground(Color.WHITE);
-
-
-        gbc.gridx = 1;  // Place the submit button in the next column
-        gbc.weightx = 0.2;
-        gbc.fill = GridBagConstraints.BOTH;
-
-    answerPanel.add(answerButton, gbc);
-
-    panel.add(answerPanel,BorderLayout.CENTER);
+        panel.add(answerPanel,BorderLayout.CENTER);
 
     JPanel buttonPanel = new JPanel();
         buttonPanel.setLayout(new GridBagLayout());
         //buttonPanel.setBorder(BorderFactory.createLineBorder(Color.BLACK,2));
         buttonPanel.setBackground(Color.LIGHT_GRAY);
 
-    JButton newQuestion = new JButton(" nexxxxt QUEEEEESTIOOON");
+        newQuestion = new JButton(" nexxxxt QUEEEEESTIOOON");
         GridBagConstraints buttonGbc = new GridBagConstraints();
-        newQuestion.setFont(new Font("Arial", Font.PLAIN, 40));
 
-        newQuestion.setBackground(Color.decode("#40629f"));
+        newQuestion.addActionListener(e ->{
+                String newQuest= generateQuestion( );
+                answerArea.setText(newQuest);
+                newQuestion.setEnabled(false);
+                newQuestion.setBackground(Color.LIGHT_GRAY);
+                answerField.setText("enter your answer here");
+                answerButton.setBackground(Color.decode("#40629f"));
+                answerButton.setText("submit");
+
+
+       });
+        newQuestion.setFont(new Font("Arial", Font.PLAIN, 40));
+        newQuestion.setEnabled(false);
+        newQuestion.setBackground(Color.LIGHT_GRAY);
         newQuestion.setForeground(Color.WHITE);
 
         buttonGbc.gridx = 0; // Place button in the first column
@@ -121,10 +84,88 @@ public class QuizWindow {
     quizFrame.setVisible(true);
 
     }
-    
-    public void Test(){
+    private JTextArea answerArea;
+    private JButton answerButton;
+    private JTextField answerField;
+    public JPanel getAnswerPanel() {
+        JPanel answerPanel = new JPanel();
+
+        answerPanel.setBackground(Color.LIGHT_GRAY);
+
+        // create grid Bag layout so i cas set up the styling
+
+        answerPanel.setLayout(new GridBagLayout());
+            GridBagConstraints gbc = new GridBagConstraints();
+            gbc.insets = new Insets(5, 5, 5, 5);
+
+        // the text area where the questions are getting into after Button click toDo
+        answerArea = new JTextArea("new Ansewers Herer");
+            answerArea.setFont(new Font("Arial", Font.PLAIN, 15));
+            answerArea.setEditable(false);
+            answerArea.setLineWrap(true);
+            answerArea.setWrapStyleWord(true);
+            answerArea.setFocusable(false);
+
+
+
+            // setting up the constraints for the Layout
+            gbc.gridx = 0;
+            gbc.gridy = 0;
+            gbc.gridwidth = 2; // Span across both columns
+            gbc.fill = GridBagConstraints.BOTH;
+            gbc.weightx = 1.0;
+            gbc.weighty = 0.90;
+
+        answerPanel.add(answerArea, gbc);
+
+        answerField = new JTextField("enter your answer here");
+
+            answerField.setFont(new Font("Arial", Font.PLAIN, 20));
+
+            gbc.gridx = 0;
+            gbc.gridy = 1; // Same row as the submit button
+            gbc.gridwidth = 1; // Span only 1 column for answer field
+            gbc.weightx = 0.8;
+            gbc.weighty=0.1;
+            gbc.fill = GridBagConstraints.BOTH;
+
+        answerPanel.add(answerField, gbc);
+
+        answerButton = new JButton("submit");
+            answerButton.addActionListener(e -> checkAswer());
+            answerButton.setFont(new Font("Arial", Font.PLAIN, 20));
+            answerButton.setBackground(Color.decode("#40629f")); // blue
+            answerButton.setForeground(Color.WHITE);
+
+
+            gbc.gridx = 1;  // Place the submit button in the next column
+            gbc.weightx = 0.2;
+            gbc.fill = GridBagConstraints.BOTH;
+
+        answerPanel.add(answerButton, gbc);
+        return answerPanel;
+    }
+        // Todo create AnswerHandeling
+
+    public void checkAswer(){
+        System.out.println("Test");
+        if (answerField.getText().contentEquals("enter your answer here")){
+            answerButton.setBackground(Color.RED);
+            answerButton.setText("Wrong answer\n Try again");
+        }
+        else{
+            answerButton.setBackground(Color.GREEN);
+            answerButton.setText("Well DONE!!");
+            newQuestion.setBackground(Color.decode("#40629f"));
+            newQuestion.setEnabled(true);
+        }
 
     }
 
+    public static String generateQuestion(){
+
+        return "Test";
+
+    }
 
 }
